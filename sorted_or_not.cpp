04dda7b743da68a36c18f8d2bcad51b6c3775fce
a2gupta <163423423+a2gupta@users.noreply.github.com>
2024-03-19 01:43:12 +0530

    Add files via upload

--- sorted_or_not.cpp
+++ sorted_or_not.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
-int sorted_or_not(int a,int n,int arr[])
+int sorted_or_not(int n,int arr[])
 {
     
     if(n==0)
-    return false;
+    return true;
 
-    if(arr[n-1]==a)
-    return (n);
-    sorted_or_not(a,n-1,arr);
+    if(arr[n]<arr[n-1])
+    return false;
+    sorted_or_not(n-1,arr);
 }
 int main()
 {
     int arr[10]={1,2,3,4,5,6,7,8,9,10};
-    cout<<sorted_or_not(5,10,arr);
+    cout<<sorted_or_not(10,arr);
 }
